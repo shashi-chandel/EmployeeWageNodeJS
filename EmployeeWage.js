2e9776dcde3b7bd1f749c8b5d07a8dacc877a5e7
Your Name <you@example.com>
2020-11-10 14:55:43 +0530

    [shashi] Added UC3: Refactored method and function added to calculate employee wage based on full or part time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -8,23 +8,26 @@ else{
     console.log("Employee is present.");
 }
 
-//  UC_2 Calculating employee wage based on part time or full time work
+// UC3 : Refactored UC2 
 {
     const IS_PART_TIME = 0;
     const IS_FULL_TIME = 1;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
-    let employeeHours = 0;
-    let employeeCheck = Math.floor(Math.random() * 10) % 2;
-    switch (employeeCheck) {
-        case IS_PART_TIME:
-            employeeHours = PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            employeeHours = FULL_TIME_HOURS;
-            break;
+    function getWorkingHours(employeeCheck) {
+        switch (employeeCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
     }
+    let employeeCheck = Math.floor(Math.random() * 10) % 3;
+    let employeeHours = getWorkingHours(employeeCheck);
     let employeeWage = employeeHours * WAGE_PER_HOUR;
-    console.log("Employee wage : $" + employeeWage);
+    console.log("Hours: "+employeeHours+" Employee wage : " + employeeWage);
 } 
+
