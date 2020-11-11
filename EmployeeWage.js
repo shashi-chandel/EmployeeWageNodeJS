6fbb8f862fa5ac3adec65776b6d2b850e2bb10bc
Your Name <you@example.com>
2020-11-11 21:00:15 +0530

    [shashi] Added UC6: Refactored method and array used to store daily wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -8,7 +8,7 @@ else{
     console.log("Employee is present.");
 }
 
-// UC5 : Refactored UC4 Monthly employee wage till max hours or max days 
+//  UC_6 Store daily wage along with total wage
 {
     const IS_PART_TIME = 0;
     const IS_FULL_TIME = 1;
@@ -17,6 +17,7 @@ else{
     const WAGE_PER_HOUR = 20;
     const NO_OF_WORKING_DAYS = 20;
     const MAX_HOURS_MONTHLY = 160;
+    let employeeDailyWage = new Array();
     function getWorkingHours(employeeCheck) {
         switch (employeeCheck) {
             case IS_PART_TIME:
@@ -27,16 +28,21 @@ else{
                 return 0;
         }
     }
+    function getWage(workingHours) {
+        return workingHours * WAGE_PER_HOUR;
+    }
     let employeeHours = 0;
     let employeeWorkingDays = 0;
     while (employeeHours <= MAX_HOURS_MONTHLY && employeeWorkingDays < NO_OF_WORKING_DAYS) {
         employeeWorkingDays++;
         let employeeCheck = Math.floor(Math.random() * 10) % 3;
         employeeHours += getWorkingHours(employeeCheck);
+	employeeDailyWage.push(getWage(getWorkingHours(employeeCheck)));    
     }
     let employeeWage = employeeHours * WAGE_PER_HOUR;
     console.log("Total Working Days : " + employeeWorkingDays);
-    console.log("Total Working Hours: "+ employeeHours +"\nEmployee wage : " + employeeWage);
+    console.log("Total Working Hours : " + employeeHours);
+    console.log("Employee wage : " + employeeWage);
 } 
 
 
